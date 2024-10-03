862ccc25e200107f81cc264db79839ac3ffdba35
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2024-10-03 14:07:31 -0400

    Task 3: Moon Orbiting complete

--- Space Shooter/Assets/Scripts/Controllers/Moon.cs
+++ Space Shooter/Assets/Scripts/Controllers/Moon.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float rotateSpeed;
+    public float rotateRadius;
+    public float tempAngle = 0;
+    Vector3 tempMoonPos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +20,17 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(rotateRadius, rotateSpeed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        tempMoonPos.x = Mathf.Cos(tempAngle * speed * Mathf.Deg2Rad);
+        tempMoonPos.y = Mathf.Sin(tempAngle * speed * Mathf.Deg2Rad);
+        transform.position = target.transform.position + tempMoonPos * radius;
+        if (tempAngle == 360 / speed)
+        {
+            tempAngle = 0;
+        } else tempAngle++;
     }
 }
